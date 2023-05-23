a9261e1f451ff7ec7384d74516c0ebb39706cc7e
Moon-jae Choi <37010595+MoonjaeChoi@users.noreply.github.com>
2023-05-23 05:17:33 +0000

    OAuth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,7 +2,6 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
 // https://console.firebase.google.com/?pli=1
-
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
